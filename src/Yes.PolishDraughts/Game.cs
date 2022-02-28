f1c0ebd0ad7fc169b8ec4c48d2447509c6f1aeec
marcellgim <marcell.gimesi@gmail.com>
2022-02-28 15:12:26 +0100

    Define game and use game class
    Instantiate in Main, ask for board size input

---
+++ src/Yes.PolishDraughts/Game.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace yes_polish_draughts
+{
+    internal class Game
+    {
+        private Board gameBoard;
+        private int player = 0;
+        public void Start()
+        {
+            int boardSize;
+            string input;
+            do
+            {
+                Console.WriteLine("Enter board size (10-20):");
+                input = Console.ReadLine() ?? String.Empty;
+
+            } while (!int.TryParse(input, out boardSize) || boardSize > 20 || boardSize < 10);
+
+            gameBoard = new Board(boardSize);
+            player = 0;
+            do
+            {
+                Round();
+            } while (!CheckForWinner());
+        }
+        private void Round()
+        {
+            // Next player
+            player = (player + 1) % 2;
+        }
+        private bool CheckForWinner()
+        {
+            return false;
+        }
+    }
+}
