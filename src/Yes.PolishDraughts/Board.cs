e614171971e0ec72b6bcbe2309fec7342d0c8b71
Csizmazia Gergely <csizmaziagergely@gmail.com>
2022-02-28 15:28:42 +0100

    Implement Board ToString

--- src/Yes.PolishDraughts/Board.cs
+++ src/Yes.PolishDraughts/Board.cs
@@ -11,24 +11,70 @@ namespace yes_polish_draughts
         public Pawn[,] Fields { get; set; }
         public Board(int size)
         {
-            Fields = new Pawn[size, size];
+            Fields = CreateBoard(size);
+        }
+
+        private Pawn[,] CreateBoard(int size)
+        {
+            var board = new Pawn[size, size];
             for (int row = 0; row < size; row++)
             {
                 for (int col = 0; col < size; col++)
                 {
-                    if ((row*size+col) % 2 == 1)
+                    if ((row + col) % 2 == 1)
                     {
                         if (row < 4)
                         {
-                            Fields[row, col] = new Pawn(1, (row, col));
+                            board[row, col] = new Pawn(1, (row, col));
                         }
-                        else if (row >= size - 4) {
-                            Fields[row, col] = new Pawn(0, (row, col));
+                        else if (row >= size - 4)
+                        {
+                            board[row, col] = new Pawn(0, (row, col));
                         }
                     }
-                    
+
                 }
             }
+            return board;
+        }
+        public override string ToString()
+        {
+            string boardString = "  ";
+            char[] alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            for (int col = 0; col < Fields.GetLength(1); col++)
+            {
+                boardString += $" {alpha[col].ToString()}";
+            }
+            boardString += "\n";
+            for (int row = 0; row < Fields.GetLength(0); row++)
+            {
+                boardString += (row+1).ToString();
+                boardString += " ";
+                if (row < 9)
+                {
+                    boardString += " ";
+                }
+                for (int col = 0; col < Fields.GetLength(1); col++)
+                {
+                    if (Fields[row, col] == null)
+                    {
+                        boardString += "  ";
+                    }
+                    else
+                    {
+                        if (Fields[row, col].Color == 0)
+                        {
+                            boardString += "><";
+                        }
+                        if (Fields[row, col].Color == 1)
+                        {
+                            boardString += "()";
+                        }
+                    }
+                }
+                boardString += "\n";
+            }
+            return boardString;
         }
     }
 }
