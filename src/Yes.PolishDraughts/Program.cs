26dc0890fe447c3366141436e818df11a6bcb554
marcellgim <marcell.gimesi@gmail.com>
2022-02-28 11:31:47 +0100

    starter repo

---
+++ src/Yes.PolishDraughts/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Yes.PolishDraughts
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
